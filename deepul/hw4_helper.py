7b9f2ddc532d45feee961a40de1619f704241a31
Alex Li <alexlioralexli@gmail.com>
2020-03-14 04:53:37 -0700

    added hw4 utils

--- deepul/hw4_helper.py
+++ deepul/hw4_helper.py
@@ -1,65 +1,125 @@
+import numpy as np
+import torchvision
+import torchvision.transforms as transforms
+
 from .utils import *
+
+
+def plot_gan_training(losses, title, fname):
+    plt.figure()
+    n_itr = len(losses)
+    xs = np.arange(n_itr)
+
+    plt.plot(xs, losses, label='loss')
+    plt.legend()
+    plt.title(title)
+    plt.xlabel('Training Iteration')
+    plt.ylabel('Loss')
+    savefig(fname)
+
+def q1_gan_plot(data, samples, xs, ys, title, fname):
+    plt.figure()
+    plt.hist(samples, bins=50, density=True, alpha=0.7, label='fake')
+    plt.hist(data, bins=50, density=True, alpha=0.7, label='real')
+
+    plt.plot(xs, ys, label='discrim')
+    plt.legend()
+    plt.title(title)
+    savefig(fname)
+
+
+######################
+##### Question 1 #####
+######################
+
+def q1_data(n=20000):
+    assert n % 2 == 0
+    gaussian1 = np.random.normal(loc=-1, scale=0.03, size=(n//2,))
+    gaussian2 = np.random.normal(loc=1, scale=0.03, size=(n//2,))
+    data = (np.concatenate([gaussian1, gaussian2]) + 1).reshape([-1, 1])
+    scaled_data = (data - np.min(data)) / (np.max(data) - np.min(data) + 1e-8)
+    return 2 * scaled_data - 1
+
+def visualize_q1_dataset():
+    data = q1_data()
+    plt.hist(data, bins=50, alpha=0.7, label='train data')
+    plt.legend()
+    plt.show()
+
+
+def q1_save_results(part, fn):
+    data = q1_data()
+    losses, samples1, xs1, ys1, samples_end, xs_end, ys_end = fn(data)
+
+    # loss plot
+    plot_gan_training(losses, 'Q1{} Losses'.format(part), 'q1{}_losses.png'.format(part))
+
+    # samples
+    q1_gan_plot(data, samples1, xs1, ys1, 'Q1{} Epoch 1'.format(part), 'q1{}_epoch1.png'.format(part))
+    q1_gan_plot(data, samples_end, xs_end, ys_end, 'Q1{} Final'.format(part), 'q1{}_final.png'.format(part))
 
 ######################
 ##### Question 2 #####
 ######################
 
+def calculate_is(samples):
+    return 0
 
+def load_q2_data():
+    train_data = torchvision.datasets.CIFAR10("./data", transform=torchvision.transforms.ToTensor())
+    return train_data
 
 def visualize_q2_data():
-    data_dir = get_data_dir(2)
-    train_data, test_data = load_pickled_data(join(data_dir, 'shapes.pkl'))
-    name = 'Shape'
-
-    idxs = np.random.choice(len(train_data), replace=False, size=(100,))
-    images = train_data[idxs] * 255
-    show_samples(images, title=f'{name} Samples')
+    train_data = load_q2_data()
+    imgs = train_data.data[:100]
+    show_samples(imgs.permute(0, 2, 3, 1) * 255.0, title=f'CIFAR-10 Samples')
 
 def q2_save_results(fn):
-    data_dir = get_data_dir(2)
-    train_data, test_data = load_pickled_data(join(data_dir, 'shapes.pkl'))
+    train_data = load_q2_data()
 
-    train_losses, test_losses, samples = fn(train_data, test_data)
-    samples = np.clip(samples.astype('float') * 2.0, 0, 1.9999)
-    floored_samples = np.floor(samples)
+    train_losses, samples = fn(train_data)
 
-    print(f'Final Test Loss: {test_losses[-1]:.4f}')
-    save_training_plot(train_losses, test_losses, f'Q2 Dataset Train Plot',
-                       f'results/q2_train_plot.png')
-    show_samples(samples * 255.0 / 2.0, f'results/q2_samples.png')
-    show_samples(floored_samples * 255.0, f'results/q2_flooredsamples.png', title='Samples with Flooring')
+    print("Inception score:", calculate_is(samples))
+    plot_gan_training(train_losses, 'Q2 Losses', 'q2_losses.png')
+    show_samples(samples[:100] * 255.0, fname='q2_samples.png', title=f'CIFAR-10 generated samples')
 
 ######################
 ##### Question 3 #####
 ######################
 
-def visualize_q3_data():
-    data_dir = get_data_dir(2)
-    train_data, test_data = load_pickled_data(join(data_dir, 'celeb.pkl'))
-    name = 'CelebA'
-
-    idxs = np.random.choice(len(train_data), replace=False, size=(100,))
-    images = train_data[idxs].astype(np.float32) / 3.0 * 255.0
-    show_samples(images, title=f'{name} Samples')
-
-def get_q3_data():
-    data_dir = get_data_dir(2)
-    train_data, test_data = load_pickled_data(join(data_dir, 'celeb.pkl'))
+def load_q3_data():
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))
+    ])
+    train_data = torchvision.datasets.MNIST(root="./data", train=True, download=True, transform=transform)
+    test_data = torchvision.datasets.MNIST(root="./data", train=Test, download=True, transform=transform)
     return train_data, test_data
 
+def visualize_q3_data():
+    train_data, _ = load_q3_data()
+    imgs = train_data.data[:100]
+    show_samples(imgs.permute(0, 2, 3, 1) * 255.0, title=f'CIFAR-10 Samples')
 
-def q3_save_results(fn, part):
-    data_dir = get_data_dir(2)
-    train_data, test_data = load_pickled_data(join(data_dir, 'celeb.pkl'))
+def plot_q3_supervised(pretrained_losses, random_losses, title, fname):
+    plt.figure()
+    xs = np.arange(len(pretrained_losses))
+    plt.plot(xs, pretrained_losses, label='bigan')
+    xs = np.arange(len(random_losses))
+    plt.plot(xs, random_losses, label='random init')
+    plt.legend()
+    plt.title(title)
+    savefig(fname)
 
-    train_losses, test_losses, samples, interpolations = fn(train_data, test_data)
-    samples = samples.astype('float')
-    interpolations = interpolations.astype('float')
+def q3_save_results(fn):
+    train_data, test_data = load_q3_data()
+    gan_losses, samples, reconstructions, pretrained_losses, random_losses = fn(train_data, test_data)
 
-    print(f'Final Test Loss: {test_losses[-1]:.4f}')
-    save_training_plot(train_losses, test_losses, f'Q3 Dataset Train Plot',
-                       f'results/q3_{part}_train_plot.png')
-    show_samples(samples * 255.0, f'results/q3_{part}_samples.png')
-    show_samples(interpolations * 255.0, f'results/q3_{part}_interpolations.png', nrow=6, title='Interpolations')
+    plot_gan_training(gan_losses, 'Q3 Losses', 'q3_gan_losses.png')
+    plot_q3_supervised(pretrained_losses, random_losses, 'Linear classification losses', 'q3_supervised_losses.png')
+    show_samples(samples * 255.0, fname='q3_samples.png', title='BiGAN generated samples')
+    show_samples(reconstructions * 255.0, nrow=20, fname='q3_reconstructions.png', title=f'BiGAN reconstructions')
+    print('BiGAN final linear classification loss:', pretrained_losses[-1])
+    print('Random encoder linear classification loss:', random_losses[-1])
 
 
